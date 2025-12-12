6368b0fad9c841eb5b1b3f262e1c00be307c0030
omeradnan91 <48936983-omeradnan91@users.noreply.replit.com>
2025-12-12 00:52:31 +0000

    Restored to '07d9bd8f750d213933d0d0dbb68fa50f2cc3aed3'
    Replit-Restored-To: 07d9bd8f750d213933d0d0dbb68fa50f2cc3aed3

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,6 @@ import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import islamicPattern from "@assets/image_1765500336350.png";
 import Home from "@/pages/home";
 import IsraMiraj from "@/pages/isra-miraj";
 import Hijrah from "@/pages/hijrah";
@@ -42,33 +41,8 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
-        <div className="relative min-h-screen">
-          {/* Islamic geometric pattern background - dark mode */}
-          <div 
-            className="fixed inset-0 opacity-100 dark:opacity-100 pointer-events-none z-0"
-            style={{
-              backgroundImage: `url(${islamicPattern})`,
-              backgroundSize: 'cover',
-              backgroundPosition: 'center',
-              backgroundRepeat: 'no-repeat'
-            }}
-          />
-          {/* Islamic geometric pattern background - light mode (inverted) */}
-          <div 
-            className="fixed inset-0 opacity-60 dark:opacity-0 pointer-events-none z-0"
-            style={{
-              backgroundImage: `url(${islamicPattern})`,
-              backgroundSize: 'cover',
-              backgroundPosition: 'center',
-              backgroundRepeat: 'no-repeat',
-              filter: 'invert(1)'
-            }}
-          />
-          <div className="relative z-10">
-            <Toaster />
-            <Router />
-          </div>
-        </div>
+        <Toaster />
+        <Router />
       </TooltipProvider>
     </QueryClientProvider>
   );
